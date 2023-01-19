c78227791d1b680c61d0bf25cfbe7412bc9b6488
Vyacheslav <slava_94@mail.ru>
2023-01-19 13:38:37 +0300

    User authorization

--- src/main/java/com/simakov/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/simakov/spring/security/configuration/MySecurityConfig.java
@@ -3,6 +3,7 @@ package com.simakov.spring.security.configuration;
 import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
 import org.springframework.security.core.userdetails.User;
 
@@ -17,7 +18,14 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
                 withUser(userBuilder.username("slava").password("slava").roles("EMPLOYEE")).
                 withUser(userBuilder.username("elena").password("elena").roles("HR")).
                 withUser(userBuilder.username("ivan").password("ivan").roles("MANAGER", "HR"));
+    }
 
-
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().
+                antMatchers("/").hasAnyRole("EMPLOYEE", "HR", "MANAGER").
+                antMatchers("/hr_info").hasRole("HR").
+                antMatchers("/manager_info").hasRole("MANAGER")
+                .and().formLogin().permitAll();
     }
 }
--- src/main/java/com/simakov/spring/security/configuration/MySecurityInitializer.java
+++ src/main/java/com/simakov/spring/security/configuration/MySecurityInitializer.java
@@ -2,9 +2,6 @@ package com.simakov.spring.security.configuration;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-
 public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
 
 }
